eb71627883664a034aab4a6ade04afd6448480ff
章子阳 <v-zhangziyang@xiaomi.com>
2022-05-12 15:13:01 +0800

    feat: 更新

--- docs/.vuepress/config/themeConfig/blogConfig.js
+++ docs/.vuepress/config/themeConfig/blogConfig.js
@@ -5,10 +5,12 @@ module.exports = {
       icon: 'reco-github',
       link: 'https://github.com/alexwjj',
       color: '#3eaf7c'
-    },{
-      icon: 'reco-juejin',
-      link: 'https://juejin.cn/user/3386151545609837',
-      color: '#00a1d6'
-    }
+    },
+    //暂时先删除掘金显示
+    // {
+    //   icon: 'reco-juejin',
+    //   link: 'https://juejin.cn/user/3386151545609837',
+    //   color: '#00a1d6'
+    // }
   ]
 }
